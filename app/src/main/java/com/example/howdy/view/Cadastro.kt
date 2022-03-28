5fd23e56095f05f530f33b7f1fe7e3d596c9dbd6
Vandão <cgomescamelo@gmail.com>
2022-03-28 11:10:44 -0300

    conexão

--- app/src/main/java/com/example/howdy/view/Cadastro.kt
+++ app/src/main/java/com/example/howdy/view/Cadastro.kt
@@ -1,13 +1,20 @@
 package com.example.howdy.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import com.example.howdy.R
 import com.example.howdy.databinding.ActivityCadastroBinding
+import com.example.howdy.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.Task
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.example.howdy.view.login as login
 
 class Cadastro : AppCompatActivity() {
 
@@ -15,10 +22,16 @@ class Cadastro : AppCompatActivity() {
     private lateinit var inputLayoutIdiomaNativo: TextInputLayout
 
     private lateinit var binding: ActivityCadastroBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.buttonCadastrar.setOnClickListener{ cadastro() }
 
         val countries = resources.getStringArray(R.array.linguagens)
         val adapter = ArrayAdapter(
@@ -36,4 +49,32 @@ class Cadastro : AppCompatActivity() {
         actionBar!!.hide()
         setContentView(R.layout.activity_cadastro)
     }
+
+    private fun cadastro() {
+        val email = binding.textEmail.text.toString()
+        val senha = binding.textSenha.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener{
+            if (it.isSuccessful){
+                val usuario = auth.currentUser
+                usuario!!.sendEmailVerification().addOnCompleteListener{
+                    if (it.isSuccessful){
+                        Toast.makeText(applicationContext,"Foi enviado o email para verificação",Toast.LENGTH_LONG).show()
+                        login()
+
+                    }else{
+                        Toast.makeText(applicationContext,"Foi enviado o email para verificação",Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private fun login(){
+        val intent = Intent(this, ActivityLoginBinding::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/example/howdy/view/login.kt
+++ app/src/main/java/com/example/howdy/view/login.kt
@@ -6,31 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.example.howdy.R
+import com.example.howdy.databinding.ActivityCadastroBinding
+import com.example.howdy.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         val actionBar = supportActionBar
         actionBar!!.hide()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
         val esqueciSenha = findViewById<TextView>(R.id.esqueci_senha)
         val registarConta = findViewById<TextView>(R.id.text_registar)
-        val buttonEntrar = findViewById<Button>(R.id.button_entrar)
 
         esqueciSenha.setOnClickListener {
             val RecuperarSenha =
                 Intent(this, RecuperarSenha::class.java)
             startActivity(RecuperarSenha)
-        }
-
-        buttonEntrar. setOnClickListener {
-
-            val intent = Intent(
-                this,
-                paginaDePostagem::class.java)
-            startActivity(intent)
-
         }
 
         registarConta.setOnClickListener {
