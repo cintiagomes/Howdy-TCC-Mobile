b8adb1038ffa268fb7f2ea0c9fde33dfd833e86e
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-04-20 11:29:42 -0300

    Get request almost finished

--- app/src/main/java/com/example/howdy/http/HttpHelper.kt
+++ app/src/main/java/com/example/howdy/http/HttpHelper.kt
@@ -1,12 +1,13 @@
 package com.example.howdy.http
 
+import com.google.firebase.auth.GetTokenResult
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 
 class HttpHelper {
-    private val serverDomain:String = "http://ipDaMaquina:3333"
+    private val serverDomain:String = "http://10.107.144.17:3333"
 
     fun post(json: String, route:String ):String{
         //DEFINIR URL DO SERVIDOR
@@ -30,24 +31,26 @@ class HttpHelper {
         return response.body().toString()
     }
 
-    /*fun get(route:String ):String{
+    /*fun get(route:String, idToken: GetTokenResult ):String{
         //DEFINIR URL DO SERVIDOR
         val url = "$serverDomain$route"
-
-        // DEFINIR O CABEÇALHO
-        val headerHttp = MediaType.parse("application/json; charset=utf-8")
+        println("DEBUGANDO1" + url)
 
         //CRIANDO UM CLIENTE QUE IRÁ DISPARAR A REQUISIÇÃO
         val client = OkHttpClient()
-
-        //CRIANDO BODY DA REQUISIÇÃO
-        //val body = RequestBody.create(headerHttp, json)
+        println("DEBUGANDO2" + client)
 
         //CONSTRUINDO REQUISIÇÃO HTTP DO TIPO POST AO SERVIDOR
-        //val request = Request.Builder().url(url).post(body).build()
+        //val request = Request.Builder().url(url)
+        //    .addHeader("Authorization", idToken)
+        //    get().build()
+
+        //println("DEBUGANDO3" + request)
 
         //UTILIZAR O CLIENT PARA FAZER A REQUISIÇÃO E RECEBER A RESPOSTA
         //val response = client.newCall(request).execute()
+
+        //println("DEBUGANDO4" + response)
 
         //return response.body().toString()
     }*/
--- app/src/main/java/com/example/howdy/view/login.kt
+++ app/src/main/java/com/example/howdy/view/login.kt
@@ -6,10 +6,14 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import com.example.howdy.CadastroActivity
-import com.example.howdy.CadastroIncompletoActivity
 import com.example.howdy.R
 import com.example.howdy.databinding.ActivityLoginBinding
+import com.example.howdy.http.HttpHelper
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GetTokenResult
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class login : AppCompatActivity() {
 
@@ -24,10 +28,10 @@ class login : AppCompatActivity() {
         setContentView(binding.root)
 
         val esqueciSenha = findViewById<TextView>(R.id.esqueci_senha)
-        val registarConta = findViewById<TextView>(R.id.text_registar)
+        val registrarConta = findViewById<TextView>(R.id.text_registar)
 
         auth = FirebaseAuth.getInstance()
-        binding.buttonEntrar.setOnClickListener { cadastrar() }
+        binding.buttonEntrar.setOnClickListener { login() }
 
         esqueciSenha.setOnClickListener {
             val RecuperarSenha =
@@ -35,7 +39,7 @@ class login : AppCompatActivity() {
             startActivity(RecuperarSenha)
         }
 
-        registarConta.setOnClickListener {
+        registrarConta.setOnClickListener {
             val ActivityTesteBinding =
                 Intent(this, CadastroActivity::class.java)
             startActivity(ActivityTesteBinding)
@@ -43,16 +47,31 @@ class login : AppCompatActivity() {
 
     }
 
-    private fun cadastrar() {
+    private fun login() {
         val email = binding.textEmail.text.toString()
         val senha = binding.textSenha.text.toString()
 
         auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener {
             if (it.isSuccessful){
-                val token = auth.currentUser?.uid
-                Toast.makeText(applicationContext,"Login executado com sucesso! e ${token}" ,
-                Toast.LENGTH_LONG).show()
-                postagem()
+                val idToken: Task<GetTokenResult>? = auth.currentUser?.getIdToken(true)
+                val uid:String? = auth.currentUser?.uid
+
+                //Tendo a certeza de que um idToken chegou
+                if (idToken != null) {
+                    //O USUÁRIO SE LOGOU NO FIREBASE, E AGORA IRÁ VER SE REALMENTE ESTÁ CADASTRADO NO BANCO SQL
+                        doAsync {
+                            val http = HttpHelper()
+                            //val result = http.get("/users/isMyUidExternalRegistered", idToken)
+
+                            uiThread {
+                                println("DEBUGANDOTOKEN" + idToken)
+                                //println("DEPOIS" + result)
+
+                                Toast.makeText(applicationContext,"Login executado com sucesso! e ${idToken}", Toast.LENGTH_LONG).show()
+                                //postagem()
+                            }
+                        }
+                }
             }else{
                 Toast.makeText(applicationContext,"Houve um erro no login", Toast.LENGTH_LONG).show()
             }
