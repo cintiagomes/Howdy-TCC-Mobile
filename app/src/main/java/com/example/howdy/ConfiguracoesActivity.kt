564afcbf01bfa52996bef894cf1b56757e0be9d1
Vandão <cgomescamelo@gmail.com>
2022-05-04 11:27:01 -0300

    Design Configurações

--- app/src/main/java/com/example/howdy/ConfiguracoesActivity.kt
+++ app/src/main/java/com/example/howdy/ConfiguracoesActivity.kt
@@ -1,7 +1,10 @@
 package com.example.howdy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.example.howdy.view.paginaDePostagem
 
 class ConfiguracoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,13 @@ class ConfiguracoesActivity : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar!!.hide()
         setContentView(R.layout.activity_configuracoes)
+
+        val arrowButton = findViewById<ImageButton>(R.id.arrow_button)
+
+        arrowButton.setOnClickListener{
+            val arrowButton =
+                Intent(this, paginaDePostagem::class.java)
+            startActivity(arrowButton)
+        }
     }
 }
