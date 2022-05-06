c479492aa50e3176ddbbbf5b0bcf28277975f3a9
Vandão <cgomescamelo@gmail.com>
2022-05-06 11:27:50 -0300

    Alert

--- app/src/main/java/com/example/howdy/ConfiguracoesActivity.kt
+++ app/src/main/java/com/example/howdy/ConfiguracoesActivity.kt
@@ -1,10 +1,16 @@
 package com.example.howdy
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.howdy.view.paginaDePostagem
+import kotlinx.android.synthetic.main.activity_recuperar_senha.*
 
 class ConfiguracoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +20,32 @@ class ConfiguracoesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_configuracoes)
 
         val arrowButton = findViewById<ImageButton>(R.id.arrow_button)
+        val buttonExcluir = findViewById<Button>(R.id.buttonExcluir)
 
         arrowButton.setOnClickListener{
             val arrowButton =
                 Intent(this, paginaDePostagem::class.java)
             startActivity(arrowButton)
         }
+
+        buttonExcluir.setOnClickListener{
+
+            val builder = AlertDialog.Builder(this, R.style.excluir)
+            builder.setTitle("EXCLUIR CONTA")
+            builder.setMessage("Deseja Excluir mesmo a conta?")
+            builder.setNegativeButton("EXCLUIR"){ dialog, with ->
+                Toast.makeText(this,"Houve um erro no login", Toast.LENGTH_LONG).show()
+            }
+            builder.setNeutralButton("CANCELAR"){dialog, with ->
+                val configuracoes =
+                    Intent(this, ConfiguracoesActivity::class.java)
+                startActivity(configuracoes)
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+
+        }
+
     }
+
 }
