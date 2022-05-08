3e64d3381fdb53b5c7dc48fd2c4e45f516ec8d4e
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-05-08 20:21:58 -0300

    :tada: Tristes tentativas falhas na busca de criar um gráfico

---
+++ app/src/main/java/com/example/howdy/ChartTestsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.howdy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChartTestsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart_tests)
+    }
+}
