f2c3d5d4144e388c9fb228be514c66e12d320f6b
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-05-12 07:47:35 -0300

    :bug: Chart correction

--- app/src/main/java/com/example/howdy/remote/APIUtil.kt
+++ app/src/main/java/com/example/howdy/remote/APIUtil.kt
@@ -1,7 +1,7 @@
 package com.example.howdy.remote
 
 object APIUtil {
-    private const val API_URL = "http://192.168.10.249:3333"
+    private const val API_URL = "http://10.107.144.24:3333"
     val `interface`: RouterInterface
         get() = RetroFitClient.getClient(API_URL)
             ?.create(RouterInterface::class.java)!!
--- app/src/main/java/com/example/howdy/view/PerfilActivity.kt
+++ app/src/main/java/com/example/howdy/view/PerfilActivity.kt
@@ -206,7 +206,7 @@ class PerfilActivity : AppCompatActivity() {
 
         //ACRESCENTANDO HISTÓRICOS DE XP NOS GRÁFICOS
 
-        val weeklyChartColumnSpacing = 8.0 / 7
+        val weeklyChartColumnSpacing = 9.0 / 7
         val weeklySeries: LineGraphSeries<DataPoint> = LineGraphSeries(
             arrayOf(
                 DataPoint(weeklyChartColumnSpacing * 0, user.xpCharts.weekly[0]),
@@ -219,7 +219,7 @@ class PerfilActivity : AppCompatActivity() {
             )
         )
 
-        val monthlySeriesChartColumnSpacing = 8.0 / 30
+        val monthlySeriesChartColumnSpacing = 9.0 / 30
         val monthlySeries: LineGraphSeries<DataPoint> = LineGraphSeries(
                 arrayOf(
                     DataPoint(monthlySeriesChartColumnSpacing * 0.0, user.xpCharts.monthly[0]),
