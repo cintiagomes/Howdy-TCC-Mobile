13ac8689524d4f01ab09ebb6286a4433cb87d08e
Vandão <cgomescamelo@gmail.com>
2022-05-24 11:28:06 -0300

    finished menu ;)

--- app/src/main/java/com/example/howdy/ChatActivity.kt
+++ app/src/main/java/com/example/howdy/ChatActivity.kt
@@ -7,6 +7,8 @@ import kotlinx.android.synthetic.main.activity_chat.*
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val actionBar = supportActionBar
+        actionBar!!.hide()
         setContentView(R.layout.activity_chat)
 
         arrow_button.setOnClickListener { finish() }
